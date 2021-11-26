d1d50e9e8ff0a7472956b7862ef0b75a13128665
Alexander <senny970@gmail.com>
2021-11-26 08:52:42 +0200

    fix loader

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="loader-wrap" v-if="isShowLoader">
-    <b-spinner variant="light" type="grow" label=""></b-spinner>
+    <b-spinner variant="light" type="grow"></b-spinner>
   </div>
 </template>
 
@@ -18,11 +18,11 @@ export default {
 <style scoped>
 .loader-wrap {
   position: fixed;
-  width: 100px;
-  height: 100px;
+  width: 100%;
+  height: 100%;
   top: 0;
   left: 0;
-  background-color: rgba(0, 0, 0, 0.7);
+  background-color: rgba(0, 0, 0, 0.8);
   display: flex;
   align-items: center;
   justify-content: center;
