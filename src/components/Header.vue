6f4ed78057ec1be0f70e4efc752bbe3b684a976a
senny970 <senny970@gmail.com>
2021-11-27 14:47:00 +0200

    add search movies feature

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,52 +3,62 @@
     <BNavbar class="navbar" type="dark" variant="dark">
       <BContainer>
         <BNavbarBrand href="#">MovieDB</BNavbarBrand>
-          <BNavForm class="nav-form">
-            <BFormInput
-                class="mr-sm-2 search-input"
-                placeholder="Search"
-                v-model="searchValue"
-            >
-            </BFormInput>
-          </BNavForm>
+        <BNavForm class="nav-form">
+          <BFormInput
+            class="mr-sm-2 search-input"
+            placeholder="Search"
+            v-model="searchValue"
+            debounce="500"
+          >
+          </BFormInput>
+        </BNavForm>
       </BContainer>
     </BNavbar>
   </header>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
   name: "Header",
   data: () => ({
-    searchValue: '',
+    searchValue: ""
   }),
   watch: {
-    searchValue: 'onSearchValueChanged',
+    searchValue: "onSearchValueChanged"
   },
   methods: {
+    ...mapActions("movies", ["searchMovies", "fetchMovies", "toggleSearchState"]),
     onSearchValueChanged(value) {
-      console.log(value);
+      if (value) {
+        this.searchMovies(value);
+        this.toggleSearchState(true);
+      } else {
+        this.fetchMovies();
+        this.toggleSearchState(false);
+      }
     }
   }
-}
+};
 </script>
 
 <style scoped>
-  .header {
-    margin-bottom: 30px;
-  }
+.header {
+  margin-bottom: 30px;
+}
 
-  .navbar {
-    background-color: rgba(0, 0, 0, 0.7) !important;
-  }
+.navbar {
+  background-color: rgba(0, 0, 0, 0.7) !important;
+}
 
-  .search-input {
-    color: #ffffff;
-    background: rgba(255, 255, 255, 0.1);
-    border-color: rgba(102, 16, 243, 1);
-  }
+.search-input {
+  color: #ffffff;
+  background: rgba(255, 255, 255, 0.1);
+  border-color: rgba(102, 16, 243, 1);
+}
 
-  .nav-form {
-    display: flex;
-  }
+.nav-form {
+  display: flex;
+}
 </style>
--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -1,6 +1,6 @@
 <template>
 <BContainer>
-  <h3 class="list-title">IMDB Top 250</h3>
+  <h3 class="list-title">{{listTitle}}</h3>
   <BRow>
     <template v-if="isExist">
       <BCol cols="3" v-for="(movie, key) in list" :key="key">
@@ -20,7 +20,7 @@
 
 <script>
 import MovieItem from "@/components/MovieItem";
-import {mapActions} from 'vuex';
+import {mapActions, mapGetters} from 'vuex';
 
 export default {
   name: "MoviesList",
@@ -34,9 +34,13 @@ export default {
     }
   },
   computed: {
+    ...mapGetters('movies', ['isSearch']),
     isExist() {
       return Boolean(Object.keys(this.list).length);
-    }
+    },
+    listTitle() {
+      return this.isSearch ? 'Search results' : 'IMDB Top 250';
+    },
   },
   methods: {
     ...mapActions('movies', ['removeMovie']),
